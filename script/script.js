604ed70488321dd90fb3e28c1994204eaff0c30f
cbssoares <dioshoaresgamer@gmail.com>
2023-03-15 14:28:17 -0300

    atuaizações para o repositorio remoto

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 let sect = [ document.querySelector('section#sect1'),document.querySelector('section#sect2')]
 let prox = document.querySelector('img#pro')
+let ante = document.querySelector('img#ant')
 let m = document.getElementById('m')
 m.removeChild(sect[1])
 var contador = 0
@@ -18,11 +19,17 @@ function ant(){
         m.appendChild(sect[contador])
     }
 }
-function blue(){
+function blue1(){
     prox.setAttribute('src', 'imagens/seta-direitaA.png')
 }
-
-function normal(){
+function blue2(){
+    ante.setAttribute('src', 'imagens/seta-esquerdaA.png')
+}
+function normal1(){
     prox.setAttribute('src', 'imagens/seta-direita.png')
+
+}
+function normal2(){
+    ante.setAttribute('src', 'imagens/seta-esquerda.png')
 
 }
